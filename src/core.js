9ff1f7a8e6d447669b011931d570b105aa1773ca
Sameer Srivastava <sameer.srivastava@nearform.com>
2022-02-03 20:45:01 +0530

    chore: add action-ref warning

---
+++ src/core.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Displays warning message if the action reference is pinned to master/main
+ *
+ * @param     repoName     Full name of the repo (owner/repo-name)
+ * @return   { String }    Warning to be emitted
+ */
+function getActionRefWarning (repoName) {
+  const actionRef = process.env.GITHUB_ACTION_REF
+  if (actionRef === 'main' || actionRef === 'master') {
+    const warning = 
+      `${repoName} is pinned at HEAD. We strongly ` +
+        `advise against pinning to "@master" as it may be unstable. Please ` +
+        `update your GitHub Action YAML from:\n\n` +
+        `    uses: '${repoName}@master'\n\n` +
+        `to:\n\n` +
+        `    uses: '${repoName}@<release/tag version>'\n\n` +
+        `Alternatively, you can pin to any git tag or git SHA in the ` +
+        `repository.`
+    return warning
+  }
+  return null
+}
+
+module.exports = {
+  getActionRefWarning
+}
---
+++ test/core.test.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const { test } = require('tap')
+const { getActionRefWarning } = require('../src/core')
+
+test('should return warning if actionRef is master', async ({ equal, plan, teardown }) => {
+  plan(1)
+
+  teardown(() => {
+    process.env.GITHUB_ACTION_REF = undefined
+  })
+  
+  process.env.GITHUB_ACTION_REF = 'master'
+  const warning = getActionRefWarning('nearform/test-repo')
+
+  const warningText = 
+  `nearform/test-repo is pinned at HEAD. We strongly ` +
+    `advise against pinning to "@master" as it may be unstable. Please ` +
+    `update your GitHub Action YAML from:\n\n` +
+    `    uses: 'nearform/test-repo@master'\n\n` +
+    `to:\n\n` +
+    `    uses: 'nearform/test-repo@<release/tag version>'\n\n` +
+    `Alternatively, you can pin to any git tag or git SHA in the ` +
+    `repository.`
+
+  equal(warning, warningText)
+})
+
+test('should return warning if actionRef is main', async ({ equal, plan, teardown }) => {
+  plan(1)
+
+  teardown(() => {
+    process.env.GITHUB_ACTION_REF = undefined
+  })
+  
+  process.env.GITHUB_ACTION_REF = 'main'
+  const warning = getActionRefWarning('nearform/test-repo')
+
+  const warningText = 
+  `nearform/test-repo is pinned at HEAD. We strongly ` +
+    `advise against pinning to "@master" as it may be unstable. Please ` +
+    `update your GitHub Action YAML from:\n\n` +
+    `    uses: 'nearform/test-repo@master'\n\n` +
+    `to:\n\n` +
+    `    uses: 'nearform/test-repo@<release/tag version>'\n\n` +
+    `Alternatively, you can pin to any git tag or git SHA in the ` +
+    `repository.`
+
+  equal(warning, warningText)
+})
+
+test('should return null if actionRef is not main or master', async ({ equal, plan, teardown }) => {
+  plan(1)
+
+  teardown(() => {
+    process.env.GITHUB_ACTION_REF = undefined
+  })
+  
+  process.env.GITHUB_ACTION_REF = 'feat-test'
+  const warning = getActionRefWarning('nearform/test-repo')
+
+  equal(warning, null)
+})
