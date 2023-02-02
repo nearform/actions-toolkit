d81cb6f073a72646bf2fdf7b5a347d6fd8b90d5f
Greg Duckworth <gregory.duckworth@nearform.com>
2023-02-02 14:33:07 +0000

    fix: only deal with nearform actions

--- src/index.js
+++ src/index.js
@@ -30,13 +30,14 @@ function logRepoWarning() {
   const actionRepo = process.env.GITHUB_ACTION_REPOSITORY
   const [repoOrg, repoName] = actionRepo.split('/')
   const newOrg = 'nearform-actions'
+  const oldOrg = 'nearform'
 
-  if (repoOrg != newOrg) {
+  if (repoOrg === oldOrg) {
     core.warning(
-      `The '${repoName}' action, no longer exists under the '${repoOrg}' organisation.\n` +
+      `The '${repoName}' action, no longer exists under the '${oldOrg}' organisation.\n` +
         `Please update it to '${newOrg}', you can do this\n` +
         `by updating your Github Workflow file from:\n\n` +
-        `    uses: '${repoOrg}/${repoName}'\n\n` +
+        `    uses: '${oldOrg}/${repoName}'\n\n` +
         `to:\n\n` +
         `    uses: '${newOrg}/${repoName}'\n\n`
     )
